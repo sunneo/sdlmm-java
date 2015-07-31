e6408657c2dd4b9f428e4f78ad37f7900b99d88c
sunneo <sunneo529@hotmail.com>
2015-07-31 23:17:11 +0800

    add hanoi tower game in example

---
+++ src/sunneo/sdlmm/exams/GUIHanoiTowerGame.java
@@ -0,0 +1,198 @@
+package sunneo.sdlmm.exams;
+
+import sunneo.sdlmm.implement.SDLMMFrame;
+
+public class GUIHanoiTowerGame extends SDLMMFrame {
+
+	static final long serialVersionUID = 1L;
+	final static int SCREEN_X = 500;
+	final static int SCREEN_Y = 400;
+
+	public static boolean checkHanoiEnd(int[] c) {
+		int len = c.length;
+		int maxValue = len + 1;
+		int[] cloneC = new int[len];
+		for (int i = 0; i < c.length; ++i) {
+			cloneC[i] = maxValue;
+			--maxValue;
+		}
+		for (int i = 0; i < c.length; ++i) {
+			if (c[i] != cloneC[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public void drawOneHanoi(int[] arr, int x, int y, int widthPerStack,
+			int heightPerStack, int stickWidth) {
+		fillRect(x, y, stickWidth, arr.length * heightPerStack, 0xff00ffff);
+		int topIdx = getTopIndex(arr);
+		int bottom = y + arr.length * heightPerStack;
+		if (topIdx == -1)
+			return;
+		for (int i = 0; i <= topIdx; ++i) {
+			int width = widthPerStack * arr[i];
+			fillRect(x - width / 2, bottom - heightPerStack * (i + 1), width,
+					heightPerStack, 0xff000000 + 0xff123 * arr[i]);
+		}
+	}
+
+	public static int[] selectTower(int selection, int[] a, int[] b, int[] c) {
+		switch (selection) {
+		case 0:
+			return a;
+		case 1:
+			return b;
+		case 2:
+			return c;
+		}
+		return null;
+	}
+
+	public static int getTopIndex(int[] tower) {
+		for (int i = tower.length - 1; i >= 0; --i) {
+			if (tower[i] != 0) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public static void moveHanoi(int from, int to, int[] a, int[] b, int[] c) {
+		int[] fromArray = selectTower(from, a, b, c);
+
+		// 用from選擇來源fromArray
+		// 用to選擇目標toArray
+		int[] toArray = selectTower(to, a, b, c);
+		// 檢查來源是否空的,空的就不搬動
+		// 取出from的頂端位置
+		int fromTopIdx = getTopIndex(fromArray);
+		if (fromTopIdx == -1) {
+			return;
+		}
+		int topValue = fromArray[fromTopIdx];
+		// 取出to的頂端位置
+		int toTopIdx = getTopIndex(toArray);
+		// 檢查fromArray頂端內容是否超過toArray的頂端,超過就不合規則
+		if (toTopIdx != -1) {
+			if (topValue > toArray[toTopIdx]) {
+				return;
+			}
+			fromArray[fromTopIdx] = 0;
+			toArray[toTopIdx + 1] = topValue;
+		} else {
+			fromArray[fromTopIdx] = 0;
+			toArray[toTopIdx + 1] = topValue;
+		}
+		// 將fromArray頂端設為0 把值移動到toArray頂端+1位置
+
+	}
+
+	public void showMessage(String str) {
+		fillRect(0, 0, 200, 30, 0xffffffff);
+		drawString(str, 0, 0, 0xff000000);
+	}
+
+	int from = -1;
+	int to = -1;
+	volatile int currentIdx = 0;
+	boolean waitSelection = false;
+	volatile int pressKey = 0;
+
+	private int showSelection(int[] xCenters, int y) {
+		if (!waitSelection)
+			return -1;
+		int centerX = xCenters[currentIdx];
+		int leftX = xCenters[currentIdx] - 10;
+		int rightX = xCenters[currentIdx] + 10;
+		drawLine(centerX, y, leftX, y + 20, 0xff0000ff);
+		drawLine(leftX, y + 20, rightX, y + 20, 0xff0000ff);
+		drawLine(centerX, y, rightX, y + 20, 0xff0000ff);
+		switch (pressKey) {
+		case 37: // left
+			currentIdx = currentIdx - 1;
+			if (currentIdx < 0)
+				currentIdx = 2;
+			break;
+		case 39: // right
+			currentIdx = currentIdx + 1;
+			if (currentIdx > 2)
+				currentIdx = 0;
+			break;
+		case 10:// enter
+		case 32:// space
+			waitSelection = false;
+			pressKey = 0;
+			return currentIdx;
+		case '1':
+		case '2':
+		case '3':
+			currentIdx = pressKey - '0';
+			break;
+		}
+		pressKey = 0;
+		return -1;
+
+	}
+
+	private int showSelection() {
+		int[] xCenters = { 100, 250, 400 };
+		return showSelection(xCenters, 250);
+	}
+
+	public void drawHanois(int[] a, int[] b, int[] c) {
+		fillRect(0, 0, SCREEN_X, SCREEN_Y, 0xffffffff);
+		drawOneHanoi(a, 100, 100, 25, 20, 5);
+		drawOneHanoi(b, 250, 100, 25, 20, 5);
+		drawOneHanoi(c, 400, 100, 25, 20, 5);
+	}
+
+	public void run() {
+		int[] a = new int[] { 5, 4, 3, 2, 1 };
+		int[] b = new int[] { 0, 0, 0, 0, 0 };
+		int[] c = new int[] { 0, 0, 0, 0, 0 };
+		while (!checkHanoiEnd(c)) {
+			drawHanois(a, b, c);
+			showMessage("請選擇搬動的來源");
+			if (from == -1) {
+				waitSelection = true;
+				from = showSelection();
+				sleep(1);
+				flush();
+				continue;
+			}
+			int n1 = from;
+			showMessage("要搬到哪去 ");
+			if (to == -1) {
+				waitSelection = true;
+				to = showSelection();
+				sleep(1);
+				flush();
+				continue;
+			}
+			int n2 = to;
+			moveHanoi(n1, n2, a, b, c);
+			from = -1;
+			to = -1;
+		}
+	}
+
+	public GUIHanoiTowerGame() {
+		super("Hanoi", SCREEN_X, SCREEN_Y);
+		setTextFont("新細明體-bold-20");
+		this.setOnKeyboard(new OnKeyboardListener() {
+			@Override
+			public void onkey(int key, boolean shift, boolean ctrl,
+					boolean alt, boolean ison) {
+				if (ison)
+					pressKey = key;
+			}
+		});
+	}
+
+	public static void main(String[] argv) {
+		GUIHanoiTowerGame game = new GUIHanoiTowerGame();
+		game.setVisible(true);
+	}
+}
--- src/sunneo/sdlmm/exams/Qsort.java
+++ src/sunneo/sdlmm/exams/Qsort.java
@@ -5,90 +5,89 @@ import java.util.Random;
 import sunneo.sdlmm.implement.SDLMMFrame;
 
 public class Qsort extends SDLMMFrame {
-    static final long serialVersionUID = 1L;
-    final static int SCREEN_X = 800;
-    final static int SCREEN_Y = 600;
-    int[] arr = new int[128];
-    int MAXDATA = 16384;
-    static Random random = new Random();
+	static final long serialVersionUID = 1L;
+	final static int SCREEN_X = 800;
+	final static int SCREEN_Y = 600;
+	int[] arr = new int[400];
+	int MAXDATA = 16384;
+	static Random random = new Random();
 
-    void showData1D(int[] data, int p1, int p2) {
-        int size = data.length;
-        int widthOfX = SCREEN_X / data.length;
-        int i;
-        fillRect(0, 0, SCREEN_X, SCREEN_Y, 0xff000000);
-        for (i = 0; i < size; ++i) {
-            int x = widthOfX * i;
-            float dataHeightRatio = ((float) data[i]) / MAXDATA;
-            int dataHeight = (int) (SCREEN_Y * dataHeightRatio);
-            int y = SCREEN_Y - dataHeight;
-            if (y < 0)
-                y = 0;
-            int color = 0xff00ffff;
-            if (i == p1) {
-                color = 0xffff0000;
-            }
-            else if (i == p2) {
-                color = 0xffffff00;
-            }
-            fillRect(x, y, widthOfX, dataHeight, color);
-        }
-        flush();
-        sleep(1);
-    }
+	void showData1D(int[] data, int p1, int p2) {
+		int size = data.length;
+		int widthOfX = SCREEN_X / data.length;
+		int i;
+		fillRect(0, 0, SCREEN_X, SCREEN_Y, 0xffffffff);
+		for (i = 0; i < size; ++i) {
+			int x = widthOfX * i;
+			float dataHeightRatio = ((float) data[i]) / MAXDATA;
+			int dataHeight = (int) (SCREEN_Y * dataHeightRatio);
+			int y = SCREEN_Y - dataHeight;
+			if (y < 0)
+				y = 0;
+			int color = 0xff000000 | ((data[i] * 0x0c0c0c) & 0x00ffffff);
+			if (i == p1) {
+				color = 0xffff0000;
+			} else if (i == p2) {
+				color = 0xffffff00;
+			}
+			fillRect(x, y, widthOfX, dataHeight, color);
+		}
+		flush();
+		sleep(1);
+	}
 
-    void init() {
-        Random r = new Random();
-        for (int i = 0; i < arr.length; ++i) {
-            arr[i] = r.nextInt(16384);
-        }
-    }
+	void init() {
+		Random r = new Random();
+		for (int i = 0; i < arr.length; ++i) {
+			arr[i] = r.nextInt(16384);
+		}
+	}
 
-    void swap(int[] arr, int i, int j) {
-        int t = arr[i];
-        arr[i] = arr[j];
-        arr[j] = t;
-    }
+	void swap(int[] arr, int i, int j) {
+		int t = arr[i];
+		arr[i] = arr[j];
+		arr[j] = t;
+	}
 
-    int partition(int arr[], int left, int right) {
-        int i = left, j = right;
-        int tmp;
-        int pivot = arr[(left + right) / 2];
-        while (i <= j) {
-            while (arr[i] < pivot)
-                i++;
-            while (arr[j] > pivot)
-                j--;
-            if (i <= j) {
-                swap(arr, i, j);
-                i++;
-                j--;
-            }
-            showData1D(arr, i, j);
-        }
-        ;
-        return i;
-    }
+	int partition(int arr[], int left, int right) {
+		int i = left, j = right;
+		int tmp;
+		int pivot = arr[(left + right) / 2];
+		while (i <= j) {
+			while (arr[i] < pivot)
+				i++;
+			while (arr[j] > pivot)
+				j--;
+			if (i <= j) {
+				swap(arr, i, j);
+				i++;
+				j--;
+			}
+			showData1D(arr, i, j);
+		}
+		;
+		return i;
+	}
 
-    void quickSort(int arr[], int left, int right) {
-        int index = partition(arr, left, right - 1);
-        if (left < index - 1)
-            quickSort(arr, left, index - 1);
-        if (index < right)
-            quickSort(arr, index, right);
-    }
+	void quickSort(int arr[], int left, int right) {
+		int index = partition(arr, left, right - 1);
+		if (left < index - 1)
+			quickSort(arr, left, index - 1);
+		if (index < right)
+			quickSort(arr, index, right);
+	}
 
-    public Qsort() {
-        super("Quick Sort", SCREEN_X, SCREEN_Y);
-        init();
-    }
+	public Qsort() {
+		super("Quick Sort", SCREEN_X, SCREEN_Y);
+		init();
+	}
 
-    public void run() {
-        quickSort(arr, 0, arr.length);
-    }
+	public void run() {
+		quickSort(arr, 0, arr.length);
+	}
 
-    public static void main(String[] argv) {
-        Qsort sort = new Qsort();
-        sort.setVisible(true);
-    }
+	public static void main(String[] argv) {
+		Qsort sort = new Qsort();
+		sort.setVisible(true);
+	}
 }
