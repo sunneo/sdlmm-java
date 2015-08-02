c829e9f6f47eca7f878a49e738e294c21373448f
sunneo <sunneo529@hotmail.com>
2015-08-02 19:04:23 +0800

    add fillPolygon, drawPolygon

--- src/sunneo/sdlmm/exams/GUIHanoiTowerGame.java
+++ src/sunneo/sdlmm/exams/GUIHanoiTowerGame.java
@@ -6,202 +6,202 @@ import sunneo.sdlmm.implement.SDLMMFrame;
 
 public class GUIHanoiTowerGame extends SDLMMFrame {
 
-	static final long serialVersionUID = 1L;
-	final static int SCREEN_X = 500;
-	final static int SCREEN_Y = 400;
+    static final long serialVersionUID = 1L;
+    final static int SCREEN_X = 500;
+    final static int SCREEN_Y = 400;
 
-	public static boolean checkHanoiEnd(int[] c) {
-		int len = c.length;
-		int maxValue = len;
-		int[] cloneC = new int[len];
-		for (int i = 0; i < c.length; ++i) {
-			cloneC[i] = maxValue;
-			--maxValue;
-		}
-		for (int i = 0; i < c.length; ++i) {
-			if (c[i] != cloneC[i]) {
-				return false;
-			}
-		}
-		return true;
-	}
+    public static boolean checkHanoiEnd(int[] c) {
+        int len = c.length;
+        int maxValue = len;
+        int[] cloneC = new int[len];
+        for (int i = 0; i < c.length; ++i) {
+            cloneC[i] = maxValue;
+            --maxValue;
+        }
+        for (int i = 0; i < c.length; ++i) {
+            if (c[i] != cloneC[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	public void drawOneHanoi(int[] arr, int x, int y, int widthPerStack,
-			int heightPerStack, int stickWidth) {
-		fillRect(x, y, stickWidth, arr.length * heightPerStack, 0xff00ffff);
-		int topIdx = getTopIndex(arr);
-		int bottom = y + arr.length * heightPerStack;
-		if (topIdx == -1)
-			return;
-		for (int i = 0; i <= topIdx; ++i) {
-			int width = widthPerStack * arr[i];
-			fillRect(x - width / 2, bottom - heightPerStack * (i + 1), width,
-					heightPerStack, 0xff000000 + 0xff123 * arr[i]);
-		}
-	}
+    public void drawOneHanoi(int[] arr, int x, int y, int widthPerStack,
+            int heightPerStack, int stickWidth) {
+        fillRect(x, y, stickWidth, arr.length * heightPerStack, 0xff00ffff);
+        int topIdx = getTopIndex(arr);
+        int bottom = y + arr.length * heightPerStack;
+        if (topIdx == -1)
+            return;
+        for (int i = 0; i <= topIdx; ++i) {
+            int width = widthPerStack * arr[i];
+            fillRect(x - width / 2, bottom - heightPerStack * (i + 1), width, heightPerStack, 0xff000000 + 0xff123 * arr[i]);
+        }
+    }
 
-	public static int[] selectTower(int selection, int[] a, int[] b, int[] c) {
-		switch (selection) {
-		case 0:
-			return a;
-		case 1:
-			return b;
-		case 2:
-			return c;
-		}
-		return null;
-	}
+    public static int[] selectTower(int selection, int[] a, int[] b, int[] c) {
+        switch (selection) {
+            case 0:
+                return a;
+            case 1:
+                return b;
+            case 2:
+                return c;
+        }
+        return null;
+    }
 
-	public static int getTopIndex(int[] tower) {
-		for (int i = tower.length - 1; i >= 0; --i) {
-			if (tower[i] != 0) {
-				return i;
-			}
-		}
-		return -1;
-	}
+    public static int getTopIndex(int[] tower) {
+        for (int i = tower.length - 1; i >= 0; --i) {
+            if (tower[i] != 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	public static void moveHanoi(int from, int to, int[] a, int[] b, int[] c) {
-		if (from == to)
-			return;
-		int[] fromArray = selectTower(from, a, b, c);
-		int[] toArray = selectTower(to, a, b, c);
-		int fromTopIdx = getTopIndex(fromArray);
-		if (fromTopIdx == -1) {
-			return;
-		}
-		int topValue = fromArray[fromTopIdx];
-		int toTopIdx = getTopIndex(toArray);
-		if (toTopIdx != -1) {
-			if (topValue > toArray[toTopIdx]) {
-				return;
-			}
-		}
-		fromArray[fromTopIdx] = 0;
-		toArray[toTopIdx + 1] = topValue;
-	}
+    public static void moveHanoi(int from, int to, int[] a, int[] b, int[] c) {
+        if (from == to)
+            return;
+        int[] fromArray = selectTower(from, a, b, c);
+        int[] toArray = selectTower(to, a, b, c);
+        int fromTopIdx = getTopIndex(fromArray);
+        if (fromTopIdx == -1) {
+            return;
+        }
+        int topValue = fromArray[fromTopIdx];
+        int toTopIdx = getTopIndex(toArray);
+        if (toTopIdx != -1) {
+            if (topValue > toArray[toTopIdx]) {
+                return;
+            }
+        }
+        fromArray[fromTopIdx] = 0;
+        toArray[toTopIdx + 1] = topValue;
+    }
 
-	public void showMessage(String str) {
-		fillRect(0, 0, 200, 30, 0xffffffff);
-		drawString(str, 0, 0, 0xff000000);
-	}
+    public void showMessage(String str) {
+        fillRect(0, 0, 200, 30, 0xffffffff);
+        drawString(str, 0, 0, 0xff000000);
+    }
 
-	int from = -1;
-	int to = -1;
-	volatile int currentIdx = 0;
-	boolean waitSelection = false;
-	volatile int pressKey = 0;
+    int from = -1;
+    int to = -1;
+    volatile int currentIdx = 0;
+    boolean waitSelection = false;
+    volatile int pressKey = 0;
 
-	private int showSelection(int[] xCenters, int y) {
-		if (!waitSelection)
-			return -1;
-		int centerX = xCenters[currentIdx];
-		int leftX = xCenters[currentIdx] - 10;
-		int rightX = xCenters[currentIdx] + 10;
-		drawLine(centerX, y, leftX, y + 20, 0xff0000ff);
-		drawLine(leftX, y + 20, rightX, y + 20, 0xff0000ff);
-		drawLine(centerX, y, rightX, y + 20, 0xff0000ff);
-		switch (pressKey) {
-		case 37: // left
-			currentIdx = currentIdx - 1;
-			if (currentIdx < 0)
-				currentIdx = 2;
-			break;
-		case 39: // right
-			currentIdx = currentIdx + 1;
-			if (currentIdx > 2)
-				currentIdx = 0;
-			break;
-		case 10:// enter
-		case 32:// space
-			waitSelection = false;
-			pressKey = 0;
-			return currentIdx;
-		case '1':
-		case '2':
-		case '3':
-			currentIdx = pressKey - '0';
-			break;
-		}
-		pressKey = 0;
-		return -1;
+    private int showSelection(int[] xCenters, int y) {
+        if (!waitSelection)
+            return -1;
+        int centerX = xCenters[currentIdx];
+        int leftX = xCenters[currentIdx] - 10;
+        int rightX = xCenters[currentIdx] + 10;
+        int[] xpt = { xCenters[currentIdx], xCenters[currentIdx] - 10,
+                xCenters[currentIdx] + 10 };
+        int[] ypt = { y, y + 20, y + 20 };
+        fillPolygon(xpt, ypt, 0xff000000);
+        switch (pressKey) {
+            case 37: // left
+                currentIdx = currentIdx - 1;
+                if (currentIdx < 0)
+                    currentIdx = 2;
+                break;
+            case 39: // right
+                currentIdx = currentIdx + 1;
+                if (currentIdx > 2)
+                    currentIdx = 0;
+                break;
+            case 10:// enter
+            case 32:// space
+                waitSelection = false;
+                pressKey = 0;
+                return currentIdx;
+            case '1':
+            case '2':
+            case '3':
+                currentIdx = pressKey - '1';
+                break;
+        }
+        pressKey = 0;
+        return -1;
 
-	}
+    }
 
-	private int showSelection() {
-		int[] xCenters = { 100, 250, 400 };
-		return showSelection(xCenters, 250);
-	}
+    private int showSelection() {
+        int[] xCenters = { 100, 250, 400 };
+        return showSelection(xCenters, 250);
+    }
 
-	int[] bg;
+    int[] bg;
 
-	long startTimeMillis = 0;
+    long startTimeMillis = 0;
 
-	public void drawHanois(int[] a, int[] b, int[] c) {
-		drawPixels(bg, 0, 0, 500, 400);
-		float timeElapsed = (((float) (System.currentTimeMillis() - startTimeMillis) / 1000));
-		drawString(String.format("時間: %4.1f 秒", timeElapsed), 200, 0,
-				0xff0000ff);
-		drawOneHanoi(a, 100, 100, 25, 20, 5);
-		drawOneHanoi(b, 250, 100, 25, 20, 5);
-		drawOneHanoi(c, 400, 100, 25, 20, 5);
-	}
+    public void drawHanois(int[] a, int[] b, int[] c) {
+        drawPixels(bg, 0, 0, 500, 400);
+        float timeElapsed = (((float) (System.currentTimeMillis() - startTimeMillis) / 1000));
+        drawString(String.format("Time Elapsed: %4.1f sec", timeElapsed), 200, 0, 0xff0000ff);
+        drawOneHanoi(a, 100, 100, 25, 20, 5);
+        drawOneHanoi(b, 250, 100, 25, 20, 5);
+        drawOneHanoi(c, 400, 100, 25, 20, 5);
+    }
 
-	public void run() {
-		int[] a = new int[] { 3, 2, 1 };
-		int[] b = new int[] { 0, 0, 0 };
-		int[] c = new int[] { 0, 0, 0 };
-		startTimeMillis = System.currentTimeMillis();
-		while (!checkHanoiEnd(c)) {
-			drawHanois(a, b, c);
-			showMessage("請選擇搬動的來源");
-			if (from == -1) {
-				waitSelection = true;
-				from = showSelection();
-				sleep(1);
-				flush();
-				continue;
-			}
-			int n1 = from;
-			showMessage("要搬到哪去 ");
-			if (to == -1) {
-				waitSelection = true;
-				to = showSelection();
-				sleep(1);
-				flush();
-				continue;
-			}
-			int n2 = to;
-			moveHanoi(n1, n2, a, b, c);
-			from = -1;
-			to = -1;
-		}
-		drawHanois(a, b, c);
-		showMessage("遊戲結束");
-		flush();
-	}
+    public void run() {
+        int[] a = new int[] { 3, 2, 1 };
+        int[] b = new int[] { 0, 0, 0 };
+        int[] c = new int[] { 0, 0, 0 };
+        startTimeMillis = System.currentTimeMillis();
+        while (!checkHanoiEnd(c)) {
+            drawHanois(a, b, c);
+            showMessage("get from which one?");
+            if (from == -1) {
+                waitSelection = true;
+                from = showSelection();
+                sleep(1);
+                flush();
+                continue;
+            }
+            int n1 = from;
+            showMessage("put to which one?");
+            if (to == -1) {
+                waitSelection = true;
+                to = showSelection();
+                sleep(1);
+                flush();
+                continue;
+            }
+            int n2 = to;
+            moveHanoi(n1, n2, a, b, c);
+            from = -1;
+            to = -1;
+        }
+        drawHanois(a, b, c);
+        showMessage("Game over");
+        flush();
+    }
 
-	public GUIHanoiTowerGame() {
-		super("Hanoi", SCREEN_X, SCREEN_Y);
-		setTextFont("新細明體-bold-20");
-		this.setOnKeyboard(new OnKeyboardListener() {
-			@Override
-			public void onkey(int key, boolean shift, boolean ctrl,
-					boolean alt, boolean ison) {
-				if (ison)
-					pressKey = key;
-			}
-		});
-		try {
-			bg = readImage("bg.bmp");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+    public GUIHanoiTowerGame() {
+        super("Hanoi", SCREEN_X, SCREEN_Y);
+        setTextFont("Arial-bold-20");
+        this.setOnKeyboard(new OnKeyboardListener() {
+            @Override
+            public void onkey(int key, boolean shift, boolean ctrl,
+                    boolean alt, boolean ison) {
+                if (ison)
+                    pressKey = key;
+            }
+        });
+        try {
+            bg = readImage("bg.bmp");
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	public static void main(String[] argv) {
-		GUIHanoiTowerGame game = new GUIHanoiTowerGame();
-		game.setVisible(true);
-	}
+    public static void main(String[] argv) {
+        GUIHanoiTowerGame game = new GUIHanoiTowerGame();
+        game.setVisible(true);
+    }
 }
--- src/sunneo/sdlmm/implement/SDLMMPanel.java
+++ src/sunneo/sdlmm/implement/SDLMMPanel.java
@@ -19,285 +19,294 @@ import javax.swing.JPanel;
 import sunneo.sdlmm.interfaces.SDLMMInterface;
 
 public abstract class SDLMMPanel extends JPanel implements SDLMMInterface {
-	/**
+    /**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-	boolean doubleBuffered;
-	OnMouseMotionListener onMouseMotion;
-	OnMousePressListener onMousePress;
-	OnMouseClickListener onMouseClick;
-	OnKeyboardListener onKeyboard;
+    private static final long serialVersionUID = 1L;
+    boolean doubleBuffered;
+    OnMouseMotionListener onMouseMotion;
+    OnMousePressListener onMousePress;
+    OnMouseClickListener onMouseClick;
+    OnKeyboardListener onKeyboard;
 
-	public void setOnMouseMotion(OnMouseMotionListener l) {
-		this.onMouseMotion = l;
+    public void setOnMouseMotion(OnMouseMotionListener l) {
+        this.onMouseMotion = l;
 
-	}
+    }
 
-	public void setOnMouseClick(OnMouseClickListener l) {
-		this.onMouseClick = l;
-	}
+    public void setOnMouseClick(OnMouseClickListener l) {
+        this.onMouseClick = l;
+    }
 
-	public void setOnMousePress(OnMousePressListener l) {
-		this.onMousePress = l;
-	}
+    public void setOnMousePress(OnMousePressListener l) {
+        this.onMousePress = l;
+    }
 
-	public void setOnKeyboard(OnKeyboardListener l) {
-		this.onKeyboard = l;
-	}
+    public void setOnKeyboard(OnKeyboardListener l) {
+        this.onKeyboard = l;
+    }
 
-	public boolean isDoubleBuffered() {
-		return doubleBuffered;
-	}
+    public boolean isDoubleBuffered() {
+        return doubleBuffered;
+    }
 
-	public void sleep(int millis) {
-		try {
-			Thread.sleep(millis);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    public void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
-	public void setTextFont(String f) {
-		this.setTextFont(Font.decode(f));
-	}
+    public void setTextFont(String f) {
+        this.setTextFont(Font.decode(f));
+    }
 
-	public String[] listFonts() {
-		return GraphicsEnvironment.getLocalGraphicsEnvironment()
-				.getAvailableFontFamilyNames();
-	}
+    public String[] listFonts() {
+        return GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+    }
 
-	private static class SDLMMPanelImpl extends SDLMMPanel {
-		BufferedImage[] image = new BufferedImage[2];
-		Graphics[] graphics = new Graphics[2];
-		int toDraw = 0;
+    private static class SDLMMPanelImpl extends SDLMMPanel {
+        BufferedImage[] image = new BufferedImage[2];
+        Graphics[] graphics = new Graphics[2];
+        int toDraw = 0;
 
-		public SDLMMPanelImpl(int width, int height, boolean doubleBuffered) {
-			setSize(width, height);
-			this.doubleBuffered = doubleBuffered;
-			image[0] = new BufferedImage(width, height,
-					BufferedImage.TYPE_4BYTE_ABGR);
-			image[1] = new BufferedImage(width, height,
-					BufferedImage.TYPE_4BYTE_ABGR);
-			graphics[0] = image[0].createGraphics();
-			graphics[1] = image[1].createGraphics();
-			this.addKeyListener(new KeyListener() {
+        public SDLMMPanelImpl(int width, int height, boolean doubleBuffered) {
+            setSize(width, height);
+            this.doubleBuffered = doubleBuffered;
+            image[0] = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
+            image[1] = new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
+            graphics[0] = image[0].createGraphics();
+            graphics[1] = image[1].createGraphics();
+            this.addKeyListener(new KeyListener() {
 
-				@Override
-				public void keyPressed(KeyEvent arg0) {
-					if (onKeyboard != null) {
-						onKeyboard.onkey(arg0.getKeyCode(), arg0.isShiftDown(),
-								arg0.isControlDown(), arg0.isAltDown(), true);
-					}
+                @Override
+                public void keyPressed(KeyEvent arg0) {
+                    if (onKeyboard != null) {
+                        onKeyboard.onkey(arg0.getKeyCode(), arg0.isShiftDown(), arg0.isControlDown(), arg0.isAltDown(), true);
+                    }
 
-				}
+                }
 
-				@Override
-				public void keyReleased(KeyEvent arg0) {
-					if (onKeyboard != null) {
-						onKeyboard.onkey(arg0.getKeyCode(), arg0.isShiftDown(),
-								arg0.isControlDown(), arg0.isAltDown(), false);
-					}
+                @Override
+                public void keyReleased(KeyEvent arg0) {
+                    if (onKeyboard != null) {
+                        onKeyboard.onkey(arg0.getKeyCode(), arg0.isShiftDown(), arg0.isControlDown(), arg0.isAltDown(), false);
+                    }
 
-				}
+                }
 
-				@Override
-				public void keyTyped(KeyEvent arg0) {
+                @Override
+                public void keyTyped(KeyEvent arg0) {
 
-				}
+                }
 
-			});
-			this.setFocusable(true);
-			this.addMouseMotionListener(new MouseMotionListener() {
+            });
+            this.setFocusable(true);
+            this.addMouseMotionListener(new MouseMotionListener() {
 
-				@Override
-				public void mouseDragged(MouseEvent arg0) {
-					if (onMouseMotion != null) {
-						onMouseMotion.onMove(arg0.getX(), arg0.getY());
-					}
-				}
+                @Override
+                public void mouseDragged(MouseEvent arg0) {
+                    if (onMouseMotion != null) {
+                        onMouseMotion.onMove(arg0.getX(), arg0.getY());
+                    }
+                }
 
-				@Override
-				public void mouseMoved(MouseEvent arg0) {
-					if (onMouseMotion != null) {
-						onMouseMotion.onMove(arg0.getX(), arg0.getY());
-					}
+                @Override
+                public void mouseMoved(MouseEvent arg0) {
+                    if (onMouseMotion != null) {
+                        onMouseMotion.onMove(arg0.getX(), arg0.getY());
+                    }
 
-				}
+                }
 
-			});
-			this.addMouseListener(new MouseListener() {
+            });
+            this.addMouseListener(new MouseListener() {
 
-				@Override
-				public void mouseClicked(MouseEvent arg0) {
-					if (onMouseClick != null) {
-						onMouseClick.onBtn(arg0.getX(), arg0.getY(),
-								arg0.getButton());
-					}
-				}
+                @Override
+                public void mouseClicked(MouseEvent arg0) {
+                    if (onMouseClick != null) {
+                        onMouseClick.onBtn(arg0.getX(), arg0.getY(), arg0.getButton());
+                    }
+                }
 
-				@Override
-				public void mouseEntered(MouseEvent arg0) {
+                @Override
+                public void mouseEntered(MouseEvent arg0) {
 
-				}
+                }
 
-				@Override
-				public void mouseExited(MouseEvent arg0) {
+                @Override
+                public void mouseExited(MouseEvent arg0) {
 
-				}
+                }
 
-				@Override
-				public void mousePressed(MouseEvent arg0) {
-					if (onMousePress != null) {
-						onMousePress.onClick(arg0.getX(), arg0.getY(),
-								arg0.getButton(), true);
-					}
+                @Override
+                public void mousePressed(MouseEvent arg0) {
+                    if (onMousePress != null) {
+                        onMousePress.onClick(arg0.getX(), arg0.getY(), arg0.getButton(), true);
+                    }
 
-				}
+                }
 
-				@Override
-				public void mouseReleased(MouseEvent arg0) {
-					if (onMousePress != null) {
-						onMousePress.onClick(arg0.getX(), arg0.getY(),
-								arg0.getButton(), false);
-					}
+                @Override
+                public void mouseReleased(MouseEvent arg0) {
+                    if (onMousePress != null) {
+                        onMousePress.onClick(arg0.getX(), arg0.getY(), arg0.getButton(), false);
+                    }
 
-				}
+                }
 
-			});
-		}
+            });
+        }
 
-		@Override
-		public void drawPixel(int x, int y, int color) {
-			graphics[toDraw].setColor(new Color(color, true));
-			graphics[toDraw].drawLine(x, y, x, y);
-			if (!this.isDoubleBuffered()) {
-				this.repaint();
-			}
-		}
+        @Override
+        public void drawPixel(int x, int y, int color) {
+            graphics[toDraw].setColor(new Color(color, true));
+            graphics[toDraw].drawLine(x, y, x, y);
+            if (!this.isDoubleBuffered()) {
+                this.repaint();
+            }
+        }
 
-		@Override
-		public void drawLine(int x1, int y1, int x2, int y2, int color) {
-			graphics[toDraw].setColor(new Color(color, true));
-			graphics[toDraw].drawLine(x1, y1, x2, y2);
-			if (!this.isDoubleBuffered()) {
-				this.repaint();
-			}
-		}
+        @Override
+        public void drawLine(int x1, int y1, int x2, int y2, int color) {
+            graphics[toDraw].setColor(new Color(color, true));
+            graphics[toDraw].drawLine(x1, y1, x2, y2);
+            if (!this.isDoubleBuffered()) {
+                this.repaint();
+            }
+        }
 
-		@Override
-		public void drawCircle(int x, int y, int r, int color) {
-			graphics[toDraw].setColor(new Color(color, true));
-			graphics[toDraw].drawOval(x - r, y - r, 2 * r, 2 * r);
-			if (!this.isDoubleBuffered()) {
-				this.repaint();
-			}
-		}
+        @Override
+        public void drawCircle(int x, int y, int r, int color) {
+            graphics[toDraw].setColor(new Color(color, true));
+            graphics[toDraw].drawOval(x - r, y - r, 2 * r, 2 * r);
+            if (!this.isDoubleBuffered()) {
+                this.repaint();
+            }
+        }
 
-		@Override
-		public void drawRect(int left, int top, int width, int height, int color) {
-			graphics[toDraw].setColor(new Color(color, true));
-			graphics[toDraw].drawRect(left, top, width, height);
-			if (!this.isDoubleBuffered()) {
-				this.repaint();
-			}
-		}
+        @Override
+        public void drawRect(int left, int top, int width, int height, int color) {
+            graphics[toDraw].setColor(new Color(color, true));
+            graphics[toDraw].drawRect(left, top, width, height);
+            if (!this.isDoubleBuffered()) {
+                this.repaint();
+            }
+        }
 
-		@Override
-		public void fillRect(int left, int top, int width, int height, int color) {
-			graphics[toDraw].setColor(new Color(color, true));
-			graphics[toDraw].fillRect(left, top, width, height);
-			if (!this.isDoubleBuffered()) {
-				this.repaint();
-			}
-		}
+        @Override
+        public void fillRect(int left, int top, int width, int height, int color) {
+            graphics[toDraw].setColor(new Color(color, true));
+            graphics[toDraw].fillRect(left, top, width, height);
+            if (!this.isDoubleBuffered()) {
+                this.repaint();
+            }
+        }
 
-		@Override
-		public void fillCircle(int x, int y, int r, int color) {
-			graphics[toDraw].setColor(new Color(color, true));
-			graphics[toDraw].fillOval(x - r, y - r, 2 * r, 2 * r);
-			if (!this.isDoubleBuffered()) {
-				this.repaint();
-			}
+        @Override
+        public void fillCircle(int x, int y, int r, int color) {
+            graphics[toDraw].setColor(new Color(color, true));
+            graphics[toDraw].fillOval(x - r, y - r, 2 * r, 2 * r);
+            if (!this.isDoubleBuffered()) {
+                this.repaint();
+            }
 
-		}
+        }
 
-		@Override
-		public void drawPixels(int[] pixels, int x, int y, int width, int height) {
-			image[toDraw].setRGB(x, y, width, height, pixels, 0, width);
+        @Override
+        public void drawPixels(int[] pixels, int x, int y, int width, int height) {
+            image[toDraw].setRGB(x, y, width, height, pixels, 0, width);
 
-			if (!this.isDoubleBuffered()) {
-				this.repaint();
-			}
-		}
+            if (!this.isDoubleBuffered()) {
+                this.repaint();
+            }
+        }
 
-		@Override
-		public void flush() {
-			if (!this.isDoubleBuffered()) {
-				return;
-			}
-			toDraw = (toDraw + 1) % 2;
-			this.repaint();
+        @Override
+        public void flush() {
+            if (!this.isDoubleBuffered()) {
+                return;
+            }
+            toDraw = (toDraw + 1) % 2;
+            this.repaint();
 
-		}
+        }
 
-		public void paint(Graphics g) {
-			// super.paint(g);
-			synchronized (g) {
-				Image img = null;
-				if (this.isDoubleBuffered()) {
-					img = image[(toDraw + 1) % 2];
-				} else {
-					img = image[toDraw];
-				}
-				g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), 0, 0,
-						this.getWidth(), this.getHeight(), null);
-			}
-		}
+        public void paint(Graphics g) {
+            // super.paint(g);
+            synchronized (g) {
+                Image img = null;
+                if (this.isDoubleBuffered()) {
+                    img = image[(toDraw + 1) % 2];
+                }
+                else {
+                    img = image[toDraw];
+                }
+                g.drawImage(img, 0, 0, this.getWidth(), this.getHeight(), 0, 0, this.getWidth(), this.getHeight(), null);
+            }
+        }
 
-		@Override
-		public int[] readImage(String filename) throws IOException {
-			BufferedImage image = javax.imageio.ImageIO
-					.read(new File(filename));
-			int[] ret = new int[image.getWidth() * image.getHeight()];
-			image.getRGB(0, 0, image.getWidth(), image.getHeight(), ret, 0,
-					image.getWidth());
-			return ret;
-		}
+        @Override
+        public int[] readImage(String filename) throws IOException {
+            BufferedImage image = javax.imageio.ImageIO.read(new File(filename));
+            int[] ret = new int[image.getWidth() * image.getHeight()];
+            image.getRGB(0, 0, image.getWidth(), image.getHeight(), ret, 0, image.getWidth());
+            return ret;
+        }
 
-		@Override
-		public void drawString(String str, int x, int y, int color) {
-			graphics[toDraw].setColor(new Color(color));
-			int size = graphics[toDraw].getFont().getSize();
-			graphics[toDraw].drawString(str, x, y + size);
-			if (!this.isDoubleBuffered()) {
-				this.repaint();
-			}
-		}
+        @Override
+        public void drawString(String str, int x, int y, int color) {
+            graphics[toDraw].setColor(new Color(color));
+            int size = graphics[toDraw].getFont().getSize();
+            graphics[toDraw].drawString(str, x, y + size);
+            if (!this.isDoubleBuffered()) {
+                this.repaint();
+            }
+        }
 
-		@Override
-		public void setTextFont(Font f) {
+        @Override
+        public void setTextFont(Font f) {
 
-			if (graphics != null) {
-				if (graphics[toDraw] != null) {
-					graphics[toDraw].setFont(f);
-				}
-				if (graphics[(toDraw + 1) % 2] != null) {
-					graphics[(toDraw + 1) % 2].setFont(f);
-				}
-			}
+            if (graphics != null) {
+                if (graphics[toDraw] != null) {
+                    graphics[toDraw].setFont(f);
+                }
+                if (graphics[(toDraw + 1) % 2] != null) {
+                    graphics[(toDraw + 1) % 2].setFont(f);
+                }
+            }
 
-		}
+        }
 
-		@Override
-		public SDLMMInterface getInterface() {
-			return this;
-		}
+        @Override
+        public SDLMMInterface getInterface() {
+            return this;
+        }
 
-	}
+        @Override
+        public void drawPolygon(int[] x, int[] y, int color) {
+            graphics[toDraw].setColor(new Color(color, true));
+            graphics[toDraw].drawPolygon(x, y, Math.min(x.length, y.length));
+            if (!this.isDoubleBuffered()) {
+                this.repaint();
+            }
+        }
 
-	public static SDLMMPanel newInstance(int width, int height,
-			boolean isdoubleBuffered) {
-		return new SDLMMPanelImpl(width, height, isdoubleBuffered);
-	}
+        @Override
+        public void fillPolygon(int[] x, int[] y, int color) {
+            graphics[toDraw].setColor(new Color(color, true));
+            graphics[toDraw].fillPolygon(x, y, Math.min(x.length, y.length));
+            if (!this.isDoubleBuffered()) {
+                this.repaint();
+            }
+        }
+
+    }
+
+    public static SDLMMPanel newInstance(int width, int height,
+            boolean isdoubleBuffered) {
+        return new SDLMMPanelImpl(width, height, isdoubleBuffered);
+    }
 }
